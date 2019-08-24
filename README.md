4d6011bb8ab710635748fcd8e885ae3f81d88262
shaoliu wu <437664071@qq.com>
2019-08-24 23:34:23 +0800

    frist commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # pyInjectionScanner
-python 远程代码注入扫描，原项目：https://github.com/sethsec/PyCodeInjection，暂未添加扫描功能，仅对源代码进行优化
+
+原项目：https://github.com/sethsec/PyCodeInjection
+
+为了使渗透速度能跟上时代的脚步，原项目的操作速度已经无法满足撒网捕鱼的需求，然后有了弄成自动化扫描的想法。
+
+python 远程代码注入扫描，暂未添加扫描功能，仅对源代码进行优化。但目标依旧是自动化，将会在这几天内弄出扫描函数。
+
++ VulnApp 文件夹中的 main.py 为测试 web 应用程序
++ pis.py 为攻击脚本
+
+# 快速开始
+
+### 依赖
+
++ Python >= 3.6
+
+进入项目目录，使用以下命令安装依赖库
+
+```
+$ pip3 install -r requirements.txt
+```
+
+若需测试 web 应用程序，在 *VulnApp* 目录下再执行一次以上相同的操作
+
+### 使用说明
+
+**帮助内容**
+
+```
+usage: pis.py [-h] [-u URL] [-c CMD] [-p [PARAMS [PARAMS ...]]]
+              [-r REQUEST_FILE]
+
+optional arguments:
+  -h, --help            显示帮助信息
+  -u URL, --url URL     URL；在 url 中使用 "*" 或设置 `-p` 参数指定注入点
+  -c CMD, --cmd CMD     远程执行的命令
+  -p [PARAMS [PARAMS ...]], --params [PARAMS [PARAMS ...]]
+                        指定注入的参数
+  -r REQUEST_FILE, --request-file REQUEST_FILE
+                        指定请求文件；`*` 和 `-p` 参数均有效
+```
+
---
+++ VulnApp/main.py
@@ -0,0 +1,104 @@
+#!/bin/python3
+
+from io import StringIO
+
+import sys
+import tornado.ioloop
+import tornado.web
+
+INJECTION_PATH = '/injection'
+PORT = 8080
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(
+            f'''<h1 align="center">Hello world!</h1>
+<p align="center">Please enter <a href="{INJECTION_PATH}">{INJECTION_PATH}</a> for testing</p>''')
+
+
+class InjectionHandler(tornado.web.RequestHandler):
+    def get(self):
+        stdout = sys.stdout
+        sys.stdout = t_io = StringIO()
+        p1 = self.get_argument('p1', '110')
+        p2 = self.get_argument('p2', '120')
+        eval_p2 = ''
+        eval_p1 = ''
+        eval_visited = ''
+        try:
+            p1 = eval(p1)
+            if p1:
+                eval_p1 = p1
+        except Exception:
+            pass
+        try:
+            p2 = eval(p2)
+            if p2:
+                eval_p2 = p2
+        except Exception:
+            pass
+        try:
+            visited = eval(self.get_cookie('visited', '1'))
+            if visited:
+                eval_visited = visited + 1
+        except Exception:
+            pass
+
+        msg = t_io.getvalue()
+        sys.stdout = stdout
+        self.set_cookie('Visited', str(eval_visited))
+        self.write('''<h1 align="center">Welcome to test!</h1>
+<p align="center">P1 handle: {}</p>
+<p align="center">P2 handle: {}</p>
+<p align="center">Message handle: {}</p>'''.format(eval_p1, eval_p2, msg))
+
+    def post(self):
+        stdout = sys.stdout
+        sys.stdout = t_io = StringIO()
+        p1 = self.get_argument('p1', '0')
+        p2 = self.get_argument('p2', '1')
+        eval_p2 = ''
+        eval_p1 = ''
+        eval_visited = ''
+        try:
+            p1 = eval(p1)
+            if p1:
+                eval_p1 = p1
+        except Exception:
+            pass
+        try:
+            p2 = eval(p2)
+            if p2:
+                eval_p2 = p2
+        except Exception:
+            pass
+        try:
+            visited = eval(self.get_cookie('visited', '1'))
+            if visited:
+                eval_visited = visited + 1
+        except Exception:
+            pass
+
+        msg = t_io.getvalue()
+        sys.stdout = stdout
+        self.set_cookie('Visited', str(eval_visited))
+        self.write('''<h1 align="center">Welcome to test!</h1>
+<p align="center">P1 handle: {}</p>
+<p align="center">P2 handle: {}</p>
+<p align="center">Message handle: {}</p>'''.format(eval_p1, eval_p2, msg))
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/injection", InjectionHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(PORT)
+    print('Web server startup, Access to http://127.0.0.1:{}'.format(PORT))
+    tornado.ioloop.IOLoop.current().start()
+
